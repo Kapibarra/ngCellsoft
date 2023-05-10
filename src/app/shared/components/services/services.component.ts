250242cbe8a7f17f5f601fe1c7a5a5264070001a
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-05-11 02:07:18 +0300

    footer and services components

--- src/app/shared/components/services/services.component.ts
+++ src/app/shared/components/services/services.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Services {
+  id: number;
+  title: string;
+  img: string;
+  description: string;
+}
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.scss']
+  styleUrls: ['./services.component.scss'],
 })
 export class ServicesComponent implements OnInit {
+  services: Services[] = [
+    {
+      id: 1,
+      title: 'Название проекта',
+      img: 'assets/images/service01.png',
+      description:
+        'Like to know the secrets of transforming a 2-14 team into a 3x Super Bowl winning Dynasty?',
+    },
+    {
+      id: 2,
+      title: 'Название проекта',
+      img: 'assets/images/service01.png',
+      description:
+        'Like to know the secrets of transforming a 2-14 team into a 3x Super Bowl winning Dynasty?',
+    },
+    {
+      id: 3,
+      title: 'Название проекта',
+      img: 'assets/images/service01.png',
+      description:
+        'Like to know the secrets of transforming a 2-14 team into a 3x Super Bowl winning Dynasty?',
+    },
+    {
+      id: 4,
+      title: 'Название проекта',
+      img: 'assets/images/service01.png',
+      description:
+        'Like to know the secrets of transforming a 2-14 team into a 3x Super Bowl winning Dynasty?',
+    },
+    {
+      id: 5,
+      title: 'Название проекта',
+      img: 'assets/images/service01.png',
+      description:
+        'Like to know the secrets of transforming a 2-14 team into a 3x Super Bowl winning Dynasty?',
+    },
+    {
+      id: 6,
+      title: 'Название проекта',
+      img: 'assets/images/service01.png',
+      description:
+        'Like to know the secrets of transforming a 2-14 team into a 3x Super Bowl winning Dynasty?',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
