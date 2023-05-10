c80f936e8397f5174c54bd2aa5c31180840bca36
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-05-10 17:54:39 +0300

    features update

--- src/app/shared/components/features/features.component.ts
+++ src/app/shared/components/features/features.component.ts
@@ -1,12 +1,103 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+interface Features {
+  id: number;
+  title: string;
 
+  keys?: {
+    name: string;
+    image: string;
+    activeDialog?: boolean;
+  }[];
+}
 @Component({
   selector: 'app-features',
   templateUrl: './features.component.html',
   styleUrls: ['./features.component.scss'],
 })
 export class FeaturesComponent implements OnInit {
-  constructor() {}
+  activeDialog: {} = {};
 
+  features: Features[] = [
+    {
+      id: 1,
+      title: 'Функциональные возможности',
+      keys: [
+        { name: 'Название 1', image: 'url/to/image1.jpg', activeDialog: false },
+        { name: 'Название 2', image: 'url/to/image2.jpg', activeDialog: false },
+        { name: 'Название 3', image: 'url/to/image3.jpg', activeDialog: false },
+        { name: 'Название 4', image: 'url/to/image4.jpg', activeDialog: false },
+        { name: 'Название 5', image: 'url/to/image5.jpg', activeDialog: false },
+        { name: 'Название 6', image: 'url/to/image6.jpg', activeDialog: false },
+      ],
+    },
+    {
+      id: 2,
+      title: 'Режим работы',
+      keys: [
+        { name: 'Название 1', image: 'url/to/image1.jpg', activeDialog: false },
+        { name: 'Название 2', image: 'url/to/image2.jpg', activeDialog: false },
+        { name: 'Название 3', image: 'url/to/image3.jpg', activeDialog: false },
+        { name: 'Название 4', image: 'url/to/image4.jpg', activeDialog: false },
+        { name: 'Название 5', image: 'url/to/image5.jpg', activeDialog: false },
+        { name: 'Название 6', image: 'url/to/image6.jpg', activeDialog: false },
+      ],
+    },
+    {
+      id: 3,
+      title: 'Материалы',
+      keys: [
+        { name: 'Название 1', image: 'url/to/image1.jpg', activeDialog: false },
+        { name: 'Название 2', image: 'url/to/image2.jpg', activeDialog: false },
+        { name: 'Название 3', image: 'url/to/image3.jpg', activeDialog: false },
+        { name: 'Название 4', image: 'url/to/image4.jpg', activeDialog: false },
+        { name: 'Название 5', image: 'url/to/image5.jpg', activeDialog: false },
+        { name: 'Название 6', image: 'url/to/image6.jpg', activeDialog: false },
+      ],
+    },
+    {
+      id: 4,
+      title: 'Производительности и масштабирование',
+      keys: [
+        { name: 'Название 1', image: 'url/to/image1.jpg', activeDialog: false },
+        { name: 'Название 2', image: 'url/to/image2.jpg', activeDialog: false },
+        { name: 'Название 3', image: 'url/to/image3.jpg', activeDialog: false },
+        { name: 'Название 4', image: 'url/to/image4.jpg', activeDialog: false },
+        { name: 'Название 5', image: 'url/to/image5.jpg', activeDialog: false },
+        { name: 'Название 6', image: 'url/to/image6.jpg', activeDialog: false },
+      ],
+    },
+    {
+      id: 5,
+      title: 'Безопасность',
+      keys: [
+        { name: 'Название 1', image: 'url/to/image1.jpg', activeDialog: false },
+        { name: 'Название 2', image: 'url/to/image2.jpg', activeDialog: false },
+        { name: 'Название 3', image: 'url/to/image3.jpg', activeDialog: false },
+        { name: 'Название 4', image: 'url/to/image4.jpg', activeDialog: false },
+        { name: 'Название 5', image: 'url/to/image5.jpg', activeDialog: false },
+        { name: 'Название 6', image: 'url/to/image6.jpg', activeDialog: false },
+      ],
+    },
+    {
+      id: 6,
+      title: 'Совместимомсть',
+      keys: [
+        { name: 'Название 1', image: 'url/to/image1.jpg', activeDialog: false },
+        { name: 'Название 2', image: 'url/to/image2.jpg', activeDialog: false },
+        { name: 'Название 3', image: 'url/to/image3.jpg', activeDialog: false },
+        { name: 'Название 4', image: 'url/to/image4.jpg', activeDialog: false },
+        { name: 'Название 5', image: 'url/to/image5.jpg', activeDialog: false },
+        { name: 'Название 6', image: 'url/to/image6.jpg', activeDialog: false },
+      ],
+    },
+  ];
+
+  constructor() {}
+  active = false;
+  @Input() init!: boolean;
   ngOnInit(): void {}
+  toggleDialog($event: any) {
+    console.log($event);
+    this.active = !this.active;
+  }
 }
