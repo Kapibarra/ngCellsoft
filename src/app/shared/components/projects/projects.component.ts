02ba30bff56abbe0a752494717b0f471583f232b
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-05-14 15:10:41 +0300

    fixes

--- src/app/shared/components/projects/projects.component.ts
+++ src/app/shared/components/projects/projects.component.ts
@@ -1,15 +1,76 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Projects {
+  id: number;
+  title: string;
+  img: string;
+  description?: {
+    project: string;
+  }[];
+}
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
+  projects: Projects[] = [
+    {
+      id: 1,
+      title: 'Проектирование',
+      img: 'assets/images/service01.png',
+      description: [
+        {
+          project: 'Полный комплект документации (РД, ЭД)',
+        },
+        {
+          project: 'Погружение с учетом технических и бизнес-требований',
+        },
+        {
+          project: 'Анализ смежных подсистем, вопросы интеграции',
+        },
+        {
+          project: 'Компетенции в части архитектуры прикладных сервисов',
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: 'Миграция имеющихся конфигураций',
+      img: 'assets/images/service02.png',
+      description: [
+        {
+          project: 'Radware Alteon',
+        },
+        {
+          project: 'F5 BIG-IP',
+        },
+        {
+          project: 'Citrix NetScaler',
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: 'Пуско-наладочные работы',
+      img: 'assets/images/service03.png',
+    },
+    {
+      id: 4,
+      title: 'Функциональные и нагрузочные испытания',
+      img: 'assets/images/service03.png',
+    },
+    {
+      id: 5,
+      title: 'Обучение',
+      img: 'assets/images/service03.png',
+    },
+    {
+      id: 6,
+      title: 'Сервисная поддержка согласно SLA',
+      img: 'assets/images/service03.png',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
