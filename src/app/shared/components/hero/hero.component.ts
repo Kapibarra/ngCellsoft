ab92ca38eb264de50e09244e47288c3a82229933
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-05-14 16:11:05 +0300

    new fixes

--- src/app/shared/components/hero/hero.component.ts
+++ src/app/shared/components/hero/hero.component.ts
@@ -1,15 +1,16 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent implements OnInit {
+  constructor(private viewportscroller: ViewportScroller) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onClickScroll(elId: string): void {
+    this.viewportscroller.scrollToAnchor(elId);
   }
-
 }
